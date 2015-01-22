4a8b15c7f86d89fb80ded973950011f664b12fc8
Snowflake Widow <snowflake_widow@yahoo.com>
2015-01-22 19:09:17 +0100

    Create video.php

---
+++ video.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>[ snowflake Widow ]</title>
+    <link href="styles.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+     <section>
+    		<?php
+            $dir = 'vidz/';
+            $scan = scandir($dir,1);
+				      
+            for ($i=0; $i<count($scan); $i++) {
+             if ($scan[$i] != '.' && $scan[$i] != '..') {
+             echo '
+             
+						<div class="vidz"><h2 class="vH2">'.$scan[$i].'</h2>							
+							<video controls="controls" preload="metadata" height="300" width="400">
+									<source src="'.$dir . $scan[$i].'" type="video/webm" />
+									No HTML5 video support sorry !
+	 						</video>             
+             </div>';
+         
+             } 
+            }
+        ?>
+     </section>
+</body>
+</html>
